140619c8fc8b31ee6a7c56182cb498c965af85b6
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-01-17 13:38:53 -0800

    Create Get_Web_Dataframe.py

---
+++ Get_Web_Dataframe.py
@@ -0,0 +1,7 @@
+def get_web_dataframe(url, filename, sep, skip, header):
+    # Save file as local
+    urlretrieve(url, filename)
+    # Parse file into a DataFrame
+    data = pd.read_csv(filename, sep=sep, skiprows=skip, header=header)
+    df = pd.DataFrame(data)
+    return df
