ca395e1212ea27d4aad5bca345a31d34787556b6
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-01-17 14:22:08 -0800

    Update get_EDA_info.py

--- get_EDA_info.py
+++ get_EDA_info.py
@@ -5,12 +5,26 @@ def get_EDA_info(filename):
   csv = pd.read_csv(filename)
   df = pd.DataFrame(csv)
   # Print shape of df
+  print("Shape")
   print(df.shape)
+  print("---------------")
   # Print all cols in df
+  print("Columns")
   print(df.columns)
+  print("---------------")
   # Print the top results of df cols
+  print("Head")
   print(df.head())
+  print("---------------")
   # Print the bottom results of df cols
+  print("Tail")
   print(df.tail())
+  print("---------------")
   # Count and print null values
   print("Total Missing Values: " + df.isnull().sum())
+  print("---------------")
+  # Print df info  
+  print("Info")
+  print(df.info())
+  print("---------------")
+
