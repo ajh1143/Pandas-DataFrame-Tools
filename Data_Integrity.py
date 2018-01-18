31415b0a106931e7f54f46ad9c96aa8b0439790f
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-01-18 15:48:07 -0800

    Create Data_Integrity.py

---
+++ Data_Integrity.py
@@ -0,0 +1,16 @@
+#REPLACES NA OBSERVATIONS IN COLUMN = colname OF DATAFRAME = df WITH MEAN OF POPULATED COLUMN
+def replace_with_mean(df, colname):
+    col_mean = np.mean(df[colname])
+    df[colname] = df[colname].fillna(col_mean)
+    print(df.info())
+    return df
+    
+    
+
+#DROP DUPLICATES FROM A DEFINED COLUMN = cols OF DATAFRAME = df
+def drop_dups(df, cols):
+    pre_col =  pd.DataFrame(df[cols])
+    print(pre_col.info())
+    post_col = pre_col.drop_duplicates()
+    print(post_col.info())
+    return post_col
