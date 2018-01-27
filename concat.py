cd1ec1d830115da73b22bae052f8321760331712
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-01-27 10:04:15 -0800

    Update concat.py

--- concat.py
+++ concat.py
@@ -1,3 +1,11 @@
+#--------------------
+#Language: Python
+#Function: concat()
+#Purpose: Concatenates a series of Pandas DataFrames
+#Inputs: list of Pandas DataFrames
+#Outputs: Concatenated DataFrame
+#--------------------
+
 def concat(df_list):
     df_output = pd.concat(df_list)
     print(df_output.shape)
