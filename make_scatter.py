745d4f9b3e6489095afdb6fc4f30b946681223f5
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-01-27 10:11:34 -0800

    Update make_scatter.py

--- make_scatter.py
+++ make_scatter.py
@@ -1,6 +1,14 @@
 #Generate a standard scatter plot from Pandas DataFrame
 #Define x and y columns and associated labels, a graph title
 
+#--------------------
+#Language: Python
+#Function: make_scatter()
+#Purpose: Generate a scatter plot from given datasets 
+#Inputs: X-axis data, Y-axis data, X-Axis Label, Y-Axis Label, Graph Title
+#Outputs: Populated Scatter Plot with Labels/Title and Legend
+#--------------------
+
 def make_scatter(x, y, xlab, ylab, gtitle):
     plt.scatter(x = x, y = y)
     plt.xlabel(xlab)
