879f5029b9efc156a2034d99b74c067d53058030
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-01-27 10:13:15 -0800

    Update make_timeseries.py

--- make_timeseries.py
+++ make_timeseries.py
@@ -1,6 +1,14 @@
 #Generate a standard timeseries plot from a Pandas DataFrame
 #Define the DataFrame to use, x and y columns, labels, and title
 
+#--------------------
+#Language: Python
+#Function: make_timeseries()
+#Purpose: Generates a TimeSeries Line Plot with labels
+#Inputs: Pandas DataFrame, Independent Variable, Dependent Variable, X-Label, Y-Label, Graph Title
+#Outputs: TimeSeries LinePlot
+#--------------------
+
 def make_timeseries(df, x, y, xlab, ylab, gtitle):
     df.plot(kind='line', x=x, y=y)
     plt.xlabel(xlab)
