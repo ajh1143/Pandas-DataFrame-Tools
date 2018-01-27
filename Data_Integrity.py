a9cee478f386e491b391908741ed74993deefb95
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-01-27 10:00:01 -0800

    Update Data_Integrity.py

--- Data_Integrity.py
+++ Data_Integrity.py
@@ -1,19 +1,36 @@
-#MISSING DATA
+#--------------------
+#Language: Python
+#Function: replace_with_mean()
+#Purpose: Replaces NA observations in specified column with mean of populated column
+#Inputs: Pandas DataFrame, Target Column Name
+#Outputs: Dataframe with missing values imputed to average
+#--------------------
 
-#REPLACES NA OBSERVATIONS IN COLUMN = colname OF DATAFRAME = df WITH MEAN OF POPULATED COLUMN
 def replace_with_mean(df, colname):
     col_mean = np.mean(df[colname])
     df[colname] = df[colname].fillna(col_mean)
     print(df.info())
     return df
-    
-#ASSERT MISSING DATA
+
+#--------------------
+#Language: Python
+#Function: check_missing_data()
+#Purpose: Tests if dataframe has missing data via Assert statement
+#Inputs: Pandas DataFrame
+#Outputs: Throws error if index values are null/missing
+#--------------------
+
 def check_missing_data(df):
     assert pd.notnull(df).all().all()
 
-    
-    
-#DUPLICATE DATA
+       
+#--------------------
+#Language: Python
+#Function: drop_dups()
+#Purpose: Drops duplicates from specified column
+#Inputs: Pandas DataFrame, Target Column Name
+#Outputs: Cleaned DataFrame, duplicate entries expunged
+#--------------------
     
 #DROP DUPLICATES FROM A DEFINED COLUMN = cols OF DATAFRAME = df
 def drop_dups(df, cols):
